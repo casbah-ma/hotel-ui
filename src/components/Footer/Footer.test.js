f7b6d0efd37d6705063e9caa2f40e1b9fb532baa
redapy <baha.redaaaa@gmail.com>
2022-08-26 12:59:00 +0100

    fix tests for Layout, Navbar, and Footer components

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -4,45 +4,44 @@ import * as stories from './Footer.stories'
 
 const { Default } = composeStories(stories)
 
-it.todo('Footer')
-// describe('Footer component', () => {
-//   const handleSubmit = jest.fn()
-//   const handleInputChange = jest.fn()
+describe('Footer component', () => {
+  const handleSubmit = jest.fn()
+  const handleInputChange = jest.fn()
 
-//   it('should render correctly', () => {
-//     render(<Default />)
-//     const footerContainer = screen.getByTestId('footer-container')
-//     expect(footerContainer).toBeInTheDocument()
-//   })
+  it('should render correctly', () => {
+    render(<Default />)
+    const footerContainer = screen.getByTestId('footer-container')
+    expect(footerContainer).toBeInTheDocument()
+  })
 
-//   it('should render correctly with props', () => {
-//     render(
-//       <Default
-//         handleSubmit={handleSubmit}
-//         handleInputChange={handleInputChange}
-//       />
-//     )
-//     const footerContainer = screen.getByTestId('footer-container')
-//     expect(footerContainer).toBeInTheDocument()
-//   })
+  it('should render correctly with props', () => {
+    render(
+      <Default
+        handleSubmit={handleSubmit}
+        handleInputChange={handleInputChange}
+      />
+    )
+    const footerContainer = screen.getByTestId('footer-container')
+    expect(footerContainer).toBeInTheDocument()
+  })
 
-//   it('should rendedr with links', () => {
-//     render(<Default />)
-//     const links = screen.getByTestId('links')
-//     expect(links).toBeInTheDocument()
-//   })
+  it('should rendedr with links', () => {
+    render(<Default />)
+    const links = screen.getByTestId('links')
+    expect(links).toBeInTheDocument()
+  })
 
-//   it('should call handleInputChange', () => {
-//     render(<Default />)
-//     const input = screen.getByTestId('footer-input')
-//     expect(input).toBeInTheDocument()
-//     fireEvent.change(input, { target: { value: 'test@test.com' } })
-//     expect(input).toHaveValue('test@test.com')
-//   })
+  it('should call handleInputChange', () => {
+    render(<Default />)
+    const input = screen.getByTestId('footer-input')
+    expect(input).toBeInTheDocument()
+    fireEvent.change(input, { target: { value: 'test@test.com' } })
+    expect(input).toHaveValue('test@test.com')
+  })
 
-//   it('Renders the copyright', () => {
-//     render(<Default />)
-//     const copyright = screen.getByTestId('copyright')
-//     expect(copyright).toBeInTheDocument()
-//   })
-// })
+  it('Renders the copyright', () => {
+    render(<Default />)
+    const copyright = screen.getByTestId('copyright')
+    expect(copyright).toBeInTheDocument()
+  })
+})
--- src/components/Layout/Layout.test.js
+++ src/components/Layout/Layout.test.js
@@ -1,17 +1,13 @@
-describe('Layout component', () => {
-  it.todo('create some test')
+import { render, screen } from '@testing-library/react'
+import { composeStories } from '@storybook/testing-react'
+import * as stories from './Layout.stories'
+
+const { Default } = composeStories(stories)
+
+describe('layout component', () => {
+  it('should render correctly', () => {
+    render(<Default />)
+    const layoutContainer = screen.getByTestId('layout-container')
+    expect(layoutContainer).toBeInTheDocument()
+  })
 })
-
-// import { render, screen } from '@testing-library/react'
-// import { composeStories } from '@storybook/testing-react'
-// import * as stories from './Layout.stories'
-
-// const { Default } = composeStories(stories)
-
-// describe('layout component', () => {
-//   it('should render correctly', () => {
-//     render(<Default />)
-//     const layoutContainer = screen.getByTestId('layout-container')
-//     expect(layoutContainer).toBeInTheDocument()
-//   })
-// })
