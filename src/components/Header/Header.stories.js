10f96b3a50bbf68f9e6aba5d635922559b380905
redapy <baha.redaaaa@gmail.com>
2022-08-29 18:19:16 +0100

    add stories for title and header v2

--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -7,11 +7,12 @@ export default {
 
 const Template = (args) => <Header {...args} />
 
-export const Normal = Template.bind({})
-Normal.args = {
+export const Default = Template.bind({})
+Default.args = {
   title: 'Sleep Peacefully',
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  direction: 'horizontal',
 }
 
 export const LargeTitle = Template.bind({})
@@ -19,6 +20,19 @@ LargeTitle.args = {
   title: 'Ready to explore Tangier?',
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  direction: 'horizontal',
+}
+
+export const Vertical = Template.bind({})
+Vertical.args = {
+  ...Default.args,
+  direction: 'vertical',
+}
+
+export const WithoutBorder = Template.bind({})
+WithoutBorder.args = {
+  ...Vertical.args,
+  withBorder: false,
 }
 
 export const Contact = Template.bind({})
