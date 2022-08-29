71de696c2508f81a372eb12e1db254a1cfe4e5e4
Yassir BOLLES <ysrbolles@gmail.com>
2022-08-30 00:05:28 +0100

    Fix Header spacing

--- src/components/Swipers/Swiper_v3/Swiper.styles.js
+++ src/components/Swipers/Swiper_v3/Swiper.styles.js
@@ -2,7 +2,7 @@ import tw, { styled } from 'twin.macro'
 
 export const SwiperContainer = tw.section`w-full h-full p-2 flex justify-start items-start`
 export const SwiperHeader = tw.div`mb-[9.25rem]`
-export const SwiperContent = tw.div`w-[19rem] h-full m-[4.25rem] flex flex-col items-start justify-start`
+export const SwiperContent = tw.div`w-[19rem] h-full m-[4rem] flex flex-col items-start justify-start`
 export const SwiperButtonsContainer = tw.div`w-[10rem] flex justify-between items-center`
 export const SwiperButton = styled.button(({ theme }) => [
   tw`w-16 h-16 rounded-full cursor-pointer flex items-center justify-center`,
