b2d11784f9fc576b709c75266424fd6a35c7c947
Yassir BOLLES <ysrbolles@gmail.com>
2022-08-31 12:05:14 +0100

    Add version Mobile

--- src/components/Bookings/BookingBar_v2/BookingBar.styles.js
+++ src/components/Bookings/BookingBar_v2/BookingBar.styles.js
@@ -1,10 +1,26 @@
 import tw, { styled } from 'twin.macro'
 
-export const BookngContainer = tw.section`w-[68.75rem] h-[6.75rem] rounded-3xl flex justify-between items-center shadow-card`
-export const BookingContent = tw.div`flex-1 h-full rounded-l-3xl flex items-center justify-center divide-x divide-gray-300 py-6`
-export const BookingContentLeft = tw.div`flex-1 h-full rounded-l-3xl flex items-center justify-around `
-export const BookingContentRight = tw.div`flex-1 h-full rounded-r-3xl flex items-center justify-around`
+export const BookingWrapper = tw.section`w-full h-full flex flex-col justify-center items-center relative`
 export const BookingButton = styled.button(({ theme }) => [
-  tw`w-[15.438rem] h-[6.813rem] rounded-r-3xl text-white font-bold text-center flex items-center justify-center self-end`,
-  `background-color: ${theme.colors.bg.primary};`,
+  tw`w-[10.688rem] lg:w-[15.438rem] h-[4.125rem] lg:h-[6.813rem] rounded-r-3xl text-xs lg:text-base text-white font-bold text-center flex items-center justify-center self-end`,
+  `background-color: ${theme.colors.bg.primary};
+  color: ${theme.colors.text.primary};
+  `,
 ])
+
+export const BookingMobileButton = styled.button(({ theme }) => [
+  tw`py-3 px-5 border border-gray-700 mt-4
+  font-secondary font-semibold text-sm 
+  md:text-base lg:text-xl transform hover:scale-100 
+  rounded-xl border-none max-w-[160px] min-w-min whitespace-nowrap`,
+  `background-color: ${theme.colors.bg.primary};
+  color: ${theme.colors.text.primary};
+  `,
+])
+
+export const BookngContainer = tw.section`h-[4.125rem] lg:h-[6.75rem] xs:w-[18rem] sm:w-[22.438rem] md:w-[45.5rem] lg:w-[68.75rem] rounded-3xl flex justify-between items-center shadow-card bg-white`
+
+export const BookingContent = tw.div`w-full lg:flex-1 h-full rounded-3xl md:rounded-r-none md:rounded-l-3xl 
+flex items-center justify-center divide-x divide-gray-300 py-3 md:px-2 lg:py-6`
+export const BookingContentLeft = tw.div`w-[13.75rem] md:flex-1 h-full flex items-center justify-around`
+export const BookingContentRight = tw.div`w-[8.688rem] md:w-1/3 lg:flex-1 h-full flex items-center justify-around`
--- src/components/Bookings/BookingBar_v2/index.jsx
+++ src/components/Bookings/BookingBar_v2/index.jsx
@@ -1,3 +1,4 @@
+import { useBreakpoint } from '@/hooks'
 import { Popover } from '@headlessui/react'
 import { ChevronUpIcon } from '@heroicons/react/24/solid'
 import PropTypes from 'prop-types'
@@ -7,11 +8,13 @@ import Guests from '../../Cards/GuestsCard'
 import DatePicker from '../../DatePicker'
 import Label from '../../Label'
 import {
+  BookingWrapper,
   BookingButton,
   BookingContent,
   BookngContainer,
   BookingContentLeft,
   BookingContentRight,
+  BookingMobileButton,
 } from './BookingBar.styles'
 
 function BookingBa_v2({
@@ -24,13 +27,15 @@ function BookingBa_v2({
   baseUrl,
 }) {
   const guestbtn = useRef(null) // ref for guests button
-  const { text, onClick } = buttonProps
+  const { text } = buttonProps // button props
+  const bp = useBreakpoint() // breakpoint
   const [isOpen, setIsOpen] = useState({
     dates: false,
     guests: false,
   })
 
-  const onDatesChanges = async (dates) => {
+  // handle changes of datepicker
+  const onDatesRangeChanges = async (dates) => {
     await onDatesChange(dates)
     if (dates?.startDate && dates?.endDate) {
       setIsOpen({ ...isOpen, dates: false, guests: true })
@@ -46,74 +51,109 @@ function BookingBa_v2({
       adult_count: guestValues?.adults,
       child_count: guestValues?.kids,
     }
-    window.open(bookingUrl(filters, baseUrl), '_blank')
+    onClick ? onClick() : window.open(bookingUrl(filters, baseUrl), '_blank')
   }
 
   return (
-    <Popover.Group style={{ position: 'relative' }}>
-      <BookngContainer data-testid="booking-bar">
-        <BookingContent>
-          <BookingContentLeft>
-            <Label
-              role="label"
-              labelText={bookingTitle.title_1}
-              fontSize="sm"
-              color="black"
-            />
-            <Popover>
-              <Popover.Button>
-                <Button
-                  as="div"
-                  variant="rounded"
-                  bgColor="white"
-                  Icon={ChevronUpIcon}
-                  color="black"
-                />
-              </Popover.Button>
-              <Popover.Panel className="datesPanel">
-                <DatePicker dates={dates} onDatesChange={onDatesChanges} />
-              </Popover.Panel>
-            </Popover>
-          </BookingContentLeft>
-          <BookingContentRight>
-            <Label
-              role="label"
-              labelText={bookingTitle.title_2}
-              fontSize="sm"
-              color="black"
-            />
-            <Popover>
-              <Popover.Button ref={guestbtn}>
-                <Button
-                  as="div"
-                  variant="rounded"
-                  bgColor="white"
-                  color="black"
-                  Icon={ChevronUpIcon}
-                />
-              </Popover.Button>
-              <Popover.Panel className="datesPanel">
-                <Guests
-                  centred
-                  title="Guests :"
-                  guestValues={guestValues}
-                  buttonProps={buttonProps}
-                  onGuestChange={onGuestChange}
-                />
-              </Popover.Panel>
-            </Popover>
-          </BookingContentRight>
-        </BookingContent>
-        <BookingButton
-          onClick={() => checkAvailability(dates, guestValues, baseUrl)}
-        >
-          {text}
-        </BookingButton>
-      </BookngContainer>
+    <Popover.Group>
+      <BookingWrapper>
+        <BookngContainer data-testid="booking-bar">
+          <BookingContent>
+            <BookingContentLeft>
+              <Label
+                role="label"
+                labelText={bookingTitle.title_1}
+                fontSize={bp}
+                color="black"
+              />
+              <Popover>
+                <Popover.Button>
+                  <Button
+                    as="div"
+                    variant="rounded"
+                    bgColor="white"
+                    Icon={ChevronUpIcon}
+                    color="black"
+                  />
+                </Popover.Button>
+                <Popover.Panel className="datesPanel">
+                  <DatePicker
+                    dates={dates}
+                    onDatesChange={onDatesRangeChanges}
+                  />
+                </Popover.Panel>
+              </Popover>
+            </BookingContentLeft>
+            <BookingContentRight>
+              <Label
+                role="label"
+                labelText={bookingTitle.title_2}
+                fontSize={bp}
+                color="black"
+              />
+              <Popover>
+                <Popover.Button ref={guestbtn}>
+                  <Button
+                    as="div"
+                    variant="rounded"
+                    bgColor="white"
+                    color="black"
+                    Icon={ChevronUpIcon}
+                  />
+                </Popover.Button>
+                <Popover.Panel className="datesPanel">
+                  <Guests
+                    centred
+                    title="Guests :"
+                    guestValues={guestValues}
+                    buttonProps={buttonProps}
+                    onGuestChange={onGuestChange}
+                  />
+                </Popover.Panel>
+              </Popover>
+            </BookingContentRight>
+          </BookingContent>
+          {bp !== 'xs' && bp !== 'sm' && (
+            <BookingButton
+              onClick={() => checkAvailability(dates, guestValues, baseUrl)}
+            >
+              {text}
+            </BookingButton>
+          )}
+        </BookngContainer>
+        {(bp === 'xs' || bp === 'sm') && (
+          <BookingMobileButton
+            onClick={() => checkAvailability(dates, guestValues, baseUrl)}
+          >
+            {' '}
+            {text}
+          </BookingMobileButton>
+        )}
+      </BookingWrapper>
     </Popover.Group>
   )
 }
 
-BookingBa_v2.propTypes = {}
+BookingBa_v2.propTypes = {
+  dates: PropTypes.shape({
+    startDate: PropTypes.instanceOf(Date),
+    endDate: PropTypes.instanceOf(Date),
+  }),
+  onDatesChange: PropTypes.func,
+  guestValues: PropTypes.shape({
+    adults: PropTypes.number,
+    kids: PropTypes.number,
+  }),
+  onGuestChange: PropTypes.func,
+  bookingTitle: PropTypes.shape({
+    title_1: PropTypes.string,
+    title_2: PropTypes.string,
+  }),
+  buttonProps: PropTypes.shape({
+    text: PropTypes.string,
+    onClick: PropTypes.func,
+  }),
+  baseUrl: PropTypes.string,
+}
 
 export default BookingBa_v2
