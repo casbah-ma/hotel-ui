9f0cc5eb7e397f7cc58d6c2f062f6717cf8c9bed
Yassir BOLLES <ysrbolles@gmail.com>
2022-09-01 00:09:09 +0100

    fix content position

--- src/components/Heros/AnimatedHero_v2/AnimatedHero_v2.styles.js
+++ src/components/Heros/AnimatedHero_v2/AnimatedHero_v2.styles.js
@@ -13,12 +13,12 @@ export const AnimatedHeroImage = tw.div`w-full h-full bg-no-repeat object-cover 
 export const AnimatedHeroChildrens = styled.div(({ position }) => [
   tw`absolute w-full h-full flex flex-col items-center justify-start lg:top-[161px] gap-10 text-white`,
   position === 'start' &&
-    tw`items-start justify-start lg:top-[5.438rem] left-[5.5rem]`,
+    tw`items-start justify-start lg:top-[5.5rem] left-[5.5rem]`,
 ])
 
 export const AnimatedHeroContent = styled.div(({ position }) => [
   tw`xs:w-[17rem] sm:w-[20.563rem] md:w-[30.063rem] lg:w-[48.875rem] text-center  text-sm h-1/2 lg:h-1/3 flex flex-col items-center justify-around gap-y-10`,
-  position === 'start' && tw`items-start text-left`,
+  position === 'start' && tw`items-start text-left mt-[5.438rem]`,
 ])
 export const AnimatedHeroButtonText = tw.span`w-[157px] absolute -top-3 left-4 md:left-16 text-white text-center md:text-2xl font-indie font-normal -rotate-6`
 
