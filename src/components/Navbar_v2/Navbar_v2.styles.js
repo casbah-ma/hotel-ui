138bdca1ce5d41d2bc288df28996d45dd06855a7
redapy <baha.redaaaa@gmail.com>
2022-09-01 18:30:48 +0100

    Create Navbar_V2 , its styles and tests

---
+++ src/components/Navbar_v2/Navbar_v2.styles.js
@@ -0,0 +1,32 @@
+import tw, { styled } from 'twin.macro'
+
+export const NavbarContainer = styled.nav(({ bgColor, color, isHidden }) => [
+  isHidden && tw`invisible`,
+  tw`w-[95%] mx-auto flex flex-row items-center justify-between 
+  pl-6 pr-7 py-6 lg:pl-[4.7rem] lg:pr-10 shadow-card rounded-3xl`,
+  bgColor && `background-color: ${bgColor};`,
+  color && `color: ${color};`,
+])
+
+export const Nav = tw.nav`
+    hidden lg:flex justify-center items-center
+`
+export const LinksList = tw.ul`
+    flex flex-col w-full whitespace-nowrap gap-4
+    lg:flex lg:flex-row lg:gap-6 lg:justify-center lg:items-center  
+`
+
+export const ListItem = tw.li`
+border-b border-black pb-2 lg:border-0 cursor-pointer hover:opacity-50
+`
+
+export const LanguageMenu = tw.div`
+ flex flex-row gap-1.5 justify-center items-center
+`
+export const ButtonWithLanguages = tw.div`
+   hidden lg:flex flex-row gap-6 justify-center items-center
+`
+
+export const Menu = tw.div`
+ block lg:hidden cursor-pointer w-8 
+`
