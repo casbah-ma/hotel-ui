60f4c7f7a7c0f2148b534c4cf15247a87899ffb5
Yassir BOLLES <ysrbolles@gmail.com>
2022-09-08 12:36:07 +0100

    Footer respansive & warnings fixed

--- src/components/Footers/Footer/Footer.styles.js
+++ src/components/Footers/Footer/Footer.styles.js
@@ -21,8 +21,8 @@ export const FooterText = tw.div`w-[15.68rem] md:w-[32.813rem] text-center`
 
 export const FooterLinks = tw.ul`text-center flex flex-wrap items-center justify-center w-72 md:w-full`
 export const FooterLinkDivider = tw.div`border-solid border-2 rounded-full mx-3 md:mx-5`
-export const FooterDivider = tw.div`border-solid border w-[19.37rem] md:w-[85vw] lg:w-[90vw] mt-3 rounded-full`
-export const FooterDividerText = tw.div`p-0 w-[19.37rem]  md:w-[85vw] lg:w-[90vw] flex items-center justify-between text-xs -mt-5`
+export const FooterDivider = tw.div`border-solid border w-[16rem] sm:w-[19.37rem] md:w-[85vw] lg:w-[90vw] mt-3 rounded-full`
+export const FooterDividerText = tw.div`p-0 w-[16rem] sm:w-[19.37rem] md:w-[85vw] lg:w-[90vw] flex items-center justify-between text-xs -mt-5`
 
 export const FooterLinkWrapper = tw.div`flex justify-center items-center`
 export const FooterLink = tw.div`text-center cursor-pointer mx-auto`
