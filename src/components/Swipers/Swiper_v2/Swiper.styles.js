546d055cab8f696d570235becd4c1f831d534768
Yassir BOLLES <ysrbolles@gmail.com>
2022-09-11 16:51:48 +0100

    fix swiper navigation

--- src/components/Swipers/Swiper_v2/Swiper.styles.js
+++ src/components/Swipers/Swiper_v2/Swiper.styles.js
@@ -1,10 +1,10 @@
 import tw, { styled } from 'twin.macro'
 
-export const SwiperContainer = tw.section`w-full h-full flex flex-col justify-start items-start`
+export const SwiperContainer = tw.section`w-full h-full flex flex-col justify-start items-start gap-5`
 export const SwiperInfo = tw.div`w-full h-full flex items-start justify-start relative mb-10`
 export const SwiperContent = tw.div`w-full h-full flex items-center justify-around`
 export const SwiperNavigation = styled.div((header) => [
-  tw`w-[8rem] flex justify-between items-center self-end`,
+  tw`w-[8rem] flex justify-between items-center self-end absolute top-2 right-20`,
   header.header === true && tw`absolute -top-40 right-20`,
 ])
 export const SwiperButton = styled.button(({ theme }) => [
