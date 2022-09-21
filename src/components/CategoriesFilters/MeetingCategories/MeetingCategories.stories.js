b7031064aa0af844328c387ca7a012ab835f9983
redapy <baha.redaaaa@gmail.com>
2022-09-21 10:05:12 +0100

    simplify the MeetingCategories component props

--- src/components/CategoriesFilters/MeetingCategories/MeetingCategories.stories.js
+++ src/components/CategoriesFilters/MeetingCategories/MeetingCategories.stories.js
@@ -1,48 +1,22 @@
 import MeetingCategories from '.'
 // Stories
-import * as RoomCardStories from '@/components/Cards/RoomCard/RoomCard.stories'
-import * as BarStories from '@/components/FilterBar/FilterBar.stories'
-import * as InfoItemsStories from '@/components/InfoItems/InfoItems.stories'
 import * as HeaderStories from '@/components/Header/Header.stories'
+import data from './data'
 
 export default {
   title: 'Sections/CategoriesFilters/MeetingCategories',
   component: MeetingCategories,
 }
 
-const categoriesDetails = [
-  {
-    roomCard: { ...RoomCardStories.WithInfoItems.args },
-    infoDeatails: InfoItemsStories.Default.args.infos,
-  },
-  {
-    roomCard: { ...RoomCardStories.WithInfoItems.args },
-    infoDeatails: InfoItemsStories.Default.args.infos,
-  },
-  {
-    roomCard: { ...RoomCardStories.WithInfoItems.args },
-    infoDeatails: InfoItemsStories.Default.args.infos,
-  },
-  {
-    roomCard: { ...RoomCardStories.WithInfoItems.args },
-    infoDeatails: InfoItemsStories.Default.args.infos,
-  },
-  {
-    roomCard: { ...RoomCardStories.WithInfoItems.args },
-    infoDeatails: InfoItemsStories.Default.args.infos,
-  },
-  {
-    roomCard: { ...RoomCardStories.WithInfoItems.args },
-    infoDeatails: InfoItemsStories.Default.args.infos,
-  },
-]
-
 //stories
 const Template = (args) => <MeetingCategories {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  categoriesDetails,
   headerProps: HeaderStories.Default.args,
-  categories: BarStories.V1.args.categories,
+  categories: data,
+  link: {
+    defaultLanguage: 'en',
+    languages: ['en', 'fr', 'ar'],
+  },
 }
--- src/components/CategoriesFilters/MeetingCategories/index.jsx
+++ src/components/CategoriesFilters/MeetingCategories/index.jsx
@@ -10,18 +10,72 @@ import { SwiperSlide } from 'swiper/react'
 import { Container } from './MeetingCategories.styles'
 //helpers
 import { isEmpty } from '@/helpers/utils'
+import { useState } from 'react'
 
-function MeetingCategories({ categories, headerProps, categoriesDetails }) {
+function MeetingCategories({ categories, headerProps, link }) {
+  console.log(categories)
+  //handle active category
+  const [active, setActive] = useState('All')
+  const handleActive = (category) => {
+    setActive(category)
+  }
+  //function to get all types and how many rooms they have
+  const getTypes = (categories) => {
+    categories.length
+    const types = []
+    categories.forEach((category) => {
+      let index = types.findIndex(
+        (_category) => _category.type === category.type
+      )
+      if (index === -1) {
+        //add the category if doesn't exist
+        types.push({ type: category.type, quantity: 1 })
+      } else {
+        //increase the quantity if it does exist
+        types[index].quantity = types[index].quantity + 1
+      }
+    })
+    types.unshift({ type: 'All', quantity: categories.length })
+    return types
+  }
+  //function to get the arrray infos from the data
+  const getInfos = (category) => {
+    return Object.entries(category)
+      .filter(
+        (item) =>
+          !(item[0] === 'image' || item[0] === 'subtitle' || item[0] === 'href')
+      )
+      .map((item) => ({
+        infoName: item[0],
+        infoValue: item[1],
+      }))
+  }
+  //filter
+  const filteredCategories = categories.filter((category) =>
+    active === 'All' ? true : category.type === active
+  )
   return (
     <Container>
       <Header {...headerProps} />
-      <FilterBar categories={categories} />
+      <FilterBar
+        categories={getTypes(categories)}
+        variant="v1"
+        active={active}
+        handleActive={handleActive}
+      />
       <Swiper type="card">
-        {!isEmpty(categoriesDetails) &&
-          categoriesDetails.map((category, index) => (
+        {!isEmpty(filteredCategories) &&
+          filteredCategories.map((category, index) => (
             <SwiperSlide key={index} className="lg:w-[25rem]">
-              <RoomCard {...category.roomCard}>
-                <InfoItems infos={category.infoDeatails} />
+              <RoomCard
+                image={category.image}
+                title={category.name}
+                subtitle={category.subtitle}
+                variant="v1"
+                href={category.href}
+                link={link}
+              >
+                <InfoItems infos={getInfos(category)} />
               </RoomCard>
             </SwiperSlide>
           ))}
@@ -33,7 +87,7 @@ function MeetingCategories({ categories, headerProps, categoriesDetails }) {
 MeetingCategories.propTypes = {
   categories: PropTypes.shape(FilterBar.propTypes),
   headerProps: PropTypes.shape(Header.propTypes),
-  categoriesDetails: PropTypes.arrayOf(
+  filteredCategories: PropTypes.arrayOf(
     PropTypes.shape({
       ...RoomCard.propTypes,
       ...InfoItems.propTypes,
