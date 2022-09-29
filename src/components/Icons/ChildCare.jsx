47e9a11dc566ff78099dde89f656d3d6e442b6ec
Ysrbolles <ysrbolles@gmail.com>
2022-09-29 15:24:24 +0100

    Create more Icons

---
+++ src/components/Icons/ChildCare.jsx
@@ -0,0 +1,88 @@
+// ChildCare
+const ChildCare = function ({ width, height, color, className, style }) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      className={className}
+      style={style}
+      fill={color}
+      xmlnsDc="http://purl.org/dc/elements/1.1/"
+      xmlnsCc="http://creativecommons.org/ns#"
+      xmlnsRdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      xmlnsSvg="http://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsSodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+      xmlnsInkscape="http://www.inkscape.org/namespaces/inkscape"
+      version="1.1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 100 100"
+    >
+      <g transform="translate(0,-952.36218)">
+        <path
+          d="m 25.111407,958.36218 c -3.846544,0 -6.981024,3.14113 -6.981024,6.99579 0,3.81684 3.13448,6.95417 6.981024,6.95417 3.808842,0 6.939531,-3.13733 6.939531,-6.95417 0,-3.85466 -3.130689,-6.99579 -6.939531,-6.99579 z m 0,1.17553 c 3.167043,0 5.766519,2.60113 5.766519,5.82026 0,3.17374 -2.599476,5.77867 -5.766519,5.77867 -3.212327,0 -5.808012,-2.60493 -5.808012,-5.77867 0,-3.21913 2.595685,-5.82026 5.808012,-5.82026 z"
+          style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+          fill={color || '#000000'}
+          fill-opacity="1"
+          fill-rule="evenodd"
+          stroke="none"
+          marker="none"
+          visibility="visible"
+          display="inline"
+          overflow="visible"
+        ></path>
+        <path
+          d="m 13.105796,975.20437 -0.109842,0.44037 -5.4565165,19.85373 -0.21972,0.73397 0.732401,0 5.9692325,0.18349 -6.4819135,23.19327 -0.21972,0.7707 0.769039,0 10.5834335,0 0,22.5327 c 0,1.0982 0.427784,1.9903 1.098638,2.5688 0.67089,0.5785 1.559055,0.8808 2.416961,0.8808 0.857905,0 1.709503,-0.3023 2.380358,-0.8808 0.200551,-0.1729 0.394653,-0.3603 0.549319,-0.5871 0.153177,0.2257 0.314185,0.4147 0.512716,0.5871 0.666709,0.5791 1.52674,0.8808 2.380358,0.8808 0.853583,0 1.713614,-0.3017 2.380359,-0.8808 0.666708,-0.579 1.13524,-1.4716 1.13524,-2.5688 l 0,-22.5327 10.62007,0 0.76904,0 -0.219721,-0.7707 -6.518551,-23.19327 5.969231,-0.18349 0.76904,0 -0.219721,-0.73397 -5.493153,-19.85373 -0.109878,-0.44037 -0.439441,0 -23.071181,0 -0.476079,0 z m 0.915555,1.17434 22.192264,0 5.163555,18.71608 -5.529756,0.18349 -3.002917,-10.67917 -1.098638,0.29358 9.631311,34.31281 -32.5193745,0 9.6312765,-34.31281 -1.13524,-0.29358 -3.002918,10.67917 -5.4931185,-0.18349 5.1635555,-18.71608 z m 5.822717,44.00119 4.687476,0 0,22.5327 c 0,0.795 -0.305539,1.3199 -0.732437,1.6881 -0.426897,0.3681 -1.005519,0.5871 -1.611318,0.5871 -0.605764,0 -1.221024,-0.219 -1.647922,-0.5871 -0.426897,-0.3682 -0.695799,-0.8931 -0.695799,-1.6881 l 0,-22.5327 z m 5.859354,0 4.650839,0 0,22.5327 c 0,0.796 -0.309154,1.3205 -0.732402,1.6881 -0.423248,0.3676 -1.011614,0.5871 -1.611319,0.5871 -0.59974,0 -1.188106,-0.2195 -1.611319,-0.5871 -0.423248,-0.3676 -0.695799,-0.8921 -0.695799,-1.6881 l 0,-22.5327 z"
+          style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+          fill={color || '#000000'}
+          fill-opacity="1"
+          fill-rule="evenodd"
+          stroke="none"
+          marker="none"
+          visibility="visible"
+          display="inline"
+          overflow="visible"
+        ></path>
+        <path
+          d="m 74.333219,1000.5628 0,18.3858 18.347063,0 0,0.5872 c -0.013,0.1787 -0.0124,0.3467 -0.0366,0.5872 -0.31223,10.1965 -8.646622,18.3491 -18.896382,18.3491 -10.446944,0 -18.896385,-8.4673 -18.896385,-18.9363 0,-10.2713 8.135539,-18.6232 18.310429,-18.9363 0.234142,-0.012 0.424346,-0.026 0.585956,-0.037 z m -1.171878,1.2111 c -9.34618,0.3038 -16.80027,7.8058 -17.101983,17.1747 l 17.101983,0 z m 18.310461,18.3491 -35.412444,0 c 0.307948,9.5626 8.06818,17.1747 17.687905,17.1747 9.619866,0 17.415209,-7.6133 17.724539,-17.1747 z"
+          style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+          fill={color || '#000000'}
+          fill-opacity="1"
+          fill-rule="evenodd"
+          stroke="none"
+          marker="none"
+          visibility="visible"
+          display="inline"
+          overflow="visible"
+        ></path>
+        <path
+          d="m 84.200656,1037.2974 c 2.474676,0 4.489686,2.0252 4.489686,4.5408 0,2.4799 -2.01501,4.4991 -4.489686,4.4991 -2.510256,0 -4.531181,-2.0192 -4.531181,-4.4991 0,-2.5156 2.020925,-4.5408 4.531181,-4.5408 z m 0,1.1755 c -1.891169,0 -3.358169,1.4701 -3.358169,3.3653 0,1.8476 1.467,3.3236 3.358169,3.3236 1.843726,0 3.316646,-1.476 3.316646,-3.3236 0,-1.8952 -1.47292,-3.3653 -3.316646,-3.3653 z"
+          style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+          fill={color || '#000000'}
+          fill-opacity="1"
+          fill-rule="evenodd"
+          stroke="none"
+          marker="none"
+          visibility="visible"
+          display="inline"
+          overflow="visible"
+        ></path>
+        <path
+          d="m 63.812894,1037.2974 c 2.474681,0 4.531216,2.0178 4.531216,4.5408 0,2.4874 -2.056535,4.4991 -4.531216,4.4991 -2.474681,0 -4.531181,-2.0117 -4.531181,-4.4991 0,-2.523 2.0565,-4.5408 4.531181,-4.5408 z m 0,1.1755 c -1.843689,0 -3.358169,1.4775 -3.358169,3.3653 0,1.84 1.51448,3.3236 3.358169,3.3236 1.843723,0 3.358169,-1.4836 3.358169,-3.3236 0,-1.8878 -1.514445,-3.3653 -3.358169,-3.3653 z"
+          style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;"
+          fill={color || '#000000'}
+          fill-opacity="1"
+          fill-rule="evenodd"
+          stroke="none"
+          marker="none"
+          visibility="visible"
+          display="inline"
+          overflow="visible"
+        ></path>
+      </g>
+    </svg>
+  )
+}
+
+export default ChildCare
