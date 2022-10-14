b8b847c45057ba8f5d193135f1f85912facd1a20
redapy <baha.redaaaa@gmail.com>
2022-10-14 10:04:04 +0100

    add tests to the GuestCard component

--- src/components/Cards/GuestCard/GuestCard.test.js
+++ src/components/Cards/GuestCard/GuestCard.test.js
@@ -1,13 +1,38 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { composeStories } from '@storybook/testing-react'
 import * as stories from './GuestCard.stories'
 
 const { Default } = composeStories(stories)
+const mockedOnMinusClick = jest.fn()
+const mockedOnPlusClick = jest.fn()
 describe('Guest Card ', () => {
-  it('should render correctly', () => {
-    render(<Default />)
+  it('should render the buttons correctly', () => {
+    render(
+      <Default
+        onMinusClick={mockedOnMinusClick}
+        onPlusClick={mockedOnPlusClick}
+      />
+    )
     const DefaultContainer = screen.getByTestId('guest-card-container')
     expect(DefaultContainer).toBeInTheDocument()
+
+    //test minus button
+    const minusButton = screen.getByLabelText(
+      `decrease ${Default.args.title} amount`
+    )
+    expect(minusButton).toBeInTheDocument()
+    minusButton.click()
+    fireEvent.click(minusButton)
+    expect(mockedOnMinusClick).toBeCalled()
+
+    const plusButton = screen.getByLabelText(
+      `increase ${Default.args.title} amount`
+    )
+    expect(plusButton).toBeInTheDocument()
+    fireEvent.click(plusButton)
+    expect(mockedOnPlusClick).toBeCalledTimes(1)
+    fireEvent.click(plusButton)
+    expect(mockedOnPlusClick).toBeCalledTimes(2)
   })
   it('should render correctly with props', () => {
     render(<Default {...Default.args} />)
