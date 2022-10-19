8f25be23de26a63508639e276156133b3ece2e48
redapy <baha.redaaaa@gmail.com>
2022-10-19 16:47:45 +0100

    remove padding for paragraph and title starting from small screens

--- src/components/Paragraph/Paragraph.styles.js
+++ src/components/Paragraph/Paragraph.styles.js
@@ -3,7 +3,7 @@ import tw, { styled } from 'twin.macro'
 
 export const Description = styled.p(({ color, theme }) => [
   `font-family: ${theme.fontFamily?.secondary};`,
-  tw` font-light  whitespace-pre-line xs:p-2`,
+  tw` font-light  whitespace-pre-line xs:p-2 sm:p-0`,
   ({ fontSize }) => fontSizes[fontSize],
   color && `color: ${color}`,
 ])
