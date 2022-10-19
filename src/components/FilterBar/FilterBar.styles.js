d024064d1be47959d5e000e50f60414d1f952840
khaoulazouggar <47794335+khaoulazouggar@users.noreply.github.com>
2022-10-19 12:41:31 +0100

    add icons and fix some styling issue

--- src/components/FilterBar/FilterBar.styles.js
+++ src/components/FilterBar/FilterBar.styles.js
@@ -2,7 +2,7 @@ import tw, { styled } from 'twin.macro'
 
 export const variants = {
   v1: tw`w-[95%] justify-start border-b border-gray-300`,
-  v2: tw`w-full md:w-[70%] [&>li]:mr-3`,
+  v2: tw`w-full [&>li]:mr-3`,
   v3: tw`pb-0 mr-10`,
 }
 export const CategoriesList = styled.ul(() => [
