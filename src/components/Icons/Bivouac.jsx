fc8ba0ffbd84e7df79cbabecbcc4ffb529d04282
khaoulazouggar <47794335+khaoulazouggar@users.noreply.github.com>
2022-12-19 18:14:13 +0100

    add icons

---
+++ src/components/Icons/Bivouac.jsx
@@ -0,0 +1,16 @@
+const Bivouac = function ({ width, height, color }) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      fill={color}
+      version="1.1"
+      viewBox="0 0 1200 1200"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="m600 241.85-517.78 716.3h57.973l73.656-101.88 40.715 101.88h285.73l59.699-228.97 59.688 228.97h285.71l40.789-101.88 73.645 101.88h57.949z" />
+    </svg>
+  )
+}
+
+export default Bivouac
